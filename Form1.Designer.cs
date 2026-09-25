c112b976a1aeb20cb0793be6d056916dcd08321d
Miguel <miguelca19112009@hotmail.com>
2026-09-25 15:54:16 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace AC_1
+{
+    partial class frm1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm1));
+            this.cboFuncao = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.nudX = new System.Windows.Forms.NumericUpDown();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblcoords = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nudC = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudB = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudA = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblGrafico = new System.Windows.Forms.Label();
+            this.btnGrafico = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudX)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudA)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboFuncao
+            // 
+            this.cboFuncao.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(170)))), ((int)(((byte)(199)))));
+            this.cboFuncao.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboFuncao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.cboFuncao.FormattingEnabled = true;
+            this.cboFuncao.Items.AddRange(new object[] {
+            "Função Afim",
+            "Função Exponencial",
+            "Função Quadrática"});
+            this.cboFuncao.Location = new System.Drawing.Point(7, 25);
+            this.cboFuncao.Name = "cboFuncao";
+            this.cboFuncao.Size = new System.Drawing.Size(177, 28);
+            this.cboFuncao.TabIndex = 0;
+            this.cboFuncao.SelectedIndexChanged += new System.EventHandler(this.cboFuncao_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(141)))), ((int)(((byte)(165)))));
+            this.pictureBox1.Location = new System.Drawing.Point(429, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(620, 620);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(170)))), ((int)(((byte)(199)))));
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(273, 536);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(88, 74);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular Y";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // nudX
+            // 
+            this.nudX.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(170)))), ((int)(((byte)(199)))));
+            this.nudX.DecimalPlaces = 2;
+            this.nudX.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.nudX.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.nudX.Location = new System.Drawing.Point(19, 462);
+            this.nudX.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudX.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.nudX.Name = "nudX";
+            this.nudX.Size = new System.Drawing.Size(120, 26);
+            this.nudX.TabIndex = 4;
+            this.nudX.ValueChanged += new System.EventHandler(this.nudX_ValueChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(170)))), ((int)(((byte)(199)))));
+            this.panel1.Controls.Add(this.lblcoords);
+            this.panel1.Location = new System.Drawing.Point(12, 509);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(255, 123);
+            this.panel1.TabIndex = 5;
+            // 
+            // lblcoords
+            // 
+            this.lblcoords.AutoSize = true;
+            this.lblcoords.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcoords.Location = new System.Drawing.Point(4, 13);
+            this.lblcoords.Name = "lblcoords";
+            this.lblcoords.Size = new System.Drawing.Size(74, 20);
+            this.lblcoords.TabIndex = 0;
+            this.lblcoords.Text = "Função:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Valor de x";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // nudC
+            // 
+            this.nudC.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(170)))), ((int)(((byte)(199)))));
+            this.nudC.DecimalPlaces = 2;
+            this.nudC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.nudC.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.nudC.Location = new System.Drawing.Point(12, 294);
+            this.nudC.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudC.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.nudC.Name = "nudC";
+            this.nudC.Size = new System.Drawing.Size(120, 26);
+            this.nudC.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(2, 266);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Valor de C";
+            // 
+            // nudB
+            // 
+            this.nudB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(170)))), ((int)(((byte)(199)))));
+            this.nudB.Cursor = System.Windows.Forms.Cursors.Default;
+            this.nudB.DecimalPlaces = 2;
+            this.nudB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.nudB.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.nudB.Location = new System.Drawing.Point(9, 200);
+            this.nudB.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudB.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.nudB.Name = "nudB";
+            this.nudB.Size = new System.Drawing.Size(120, 26);
+            this.nudB.TabIndex = 9;
+            this.nudB.ValueChanged += new System.EventHandler(this.nudB_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(2, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Valor de B";
+            // 
+            // nudA
+            // 
+            this.nudA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(170)))), ((int)(((byte)(199)))));
+            this.nudA.DecimalPlaces = 2;
+            this.nudA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudA.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.nudA.Location = new System.Drawing.Point(9, 106);
+            this.nudA.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudA.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.nudA.Name = "nudA";
+            this.nudA.Size = new System.Drawing.Size(120, 26);
+            this.nudA.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(2, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 25);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Valor de A";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(170)))), ((int)(((byte)(199)))));
+            this.panel2.Controls.Add(this.lblGrafico);
+            this.panel2.Location = new System.Drawing.Point(190, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(211, 410);
+            this.panel2.TabIndex = 6;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // lblGrafico
+            // 
+            this.lblGrafico.AutoSize = true;
+            this.lblGrafico.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGrafico.Location = new System.Drawing.Point(4, 13);
+            this.lblGrafico.Name = "lblGrafico";
+            this.lblGrafico.Size = new System.Drawing.Size(62, 16);
+            this.lblGrafico.TabIndex = 0;
+            this.lblGrafico.Text = "Função:";
+            // 
+            // btnGrafico
+            // 
+            this.btnGrafico.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(170)))), ((int)(((byte)(199)))));
+            this.btnGrafico.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnGrafico.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGrafico.Location = new System.Drawing.Point(221, 428);
+            this.btnGrafico.Name = "btnGrafico";
+            this.btnGrafico.Size = new System.Drawing.Size(140, 60);
+            this.btnGrafico.TabIndex = 13;
+            this.btnGrafico.Text = "Calcular Gráficos";
+            this.btnGrafico.UseVisualStyleBackColor = false;
+            this.btnGrafico.Click += new System.EventHandler(this.btnGrafico_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(170)))), ((int)(((byte)(199)))));
+            this.pictureBox2.Location = new System.Drawing.Point(416, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(645, 645);
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // frm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(200)))), ((int)(((byte)(233)))));
+            this.ClientSize = new System.Drawing.Size(1118, 656);
+            this.Controls.Add(this.btnGrafico);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nudA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nudB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nudC);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudX);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cboFuncao);
+            this.Controls.Add(this.pictureBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frm1";
+            this.Text = "Falculo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudX)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudA)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboFuncao;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.NumericUpDown nudX;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblcoords;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudC;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudA;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblGrafico;
+        private System.Windows.Forms.Button btnGrafico;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace AC_1
+{
+    public partial class frm1 : Form
+    {
+        public frm1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboFuncao_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //seleção da funcao
+            switch (cboFuncao.SelectedIndex)
+            {
+                case 0:
+                    nudB.Minimum = -100000;
+                    nudC.Enabled = false;
+                    break;
+                case 1:
+                    nudB.Value = 1;
+                    nudB.Minimum = 0;
+                    nudC.Enabled = false;
+                    break;
+                case 2:
+                    nudB.Minimum = -100000;
+                    nudC.Enabled = true; 
+                    break;
+            }
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nudX_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            
+            string funcaoescolhida = cboFuncao.Text;
+            decimal decimalx = nudX.Value;
+            decimal decimala = nudA.Value;
+            decimal decimalb = nudB.Value;
+            decimal decimalc = nudC.Value;
+            float x = (float)decimalx, a = (float)decimala, b = (float)decimalb, c = (float)decimalc;
+            float y=0,expoY=1,QuadX=1;
+
+
+            //Numeros necessários para o cálculo das funcaes
+            
+            double dexpoY = (double)expoY;
+
+            for (int i = 0; i < 2; i++)//Para Y com X elevado por 2
+            {
+                QuadX *= x;
+            }
+
+
+            switch (cboFuncao.SelectedIndex)
+            {
+                case 0:
+                    nudB.Minimum = -10000;
+                    y = a * x + b;
+                    break;
+                case 1:
+                    
+                    dexpoY = Math.Pow(b, x)*a;
+                    break;
+                case 2:
+                    nudB.Minimum = -10000;
+                    y = a*QuadX+(b*x)+c ;
+                    break;
+            }
+            //em cada caso para calcular o x
+            if (funcaoescolhida == "Função Afim") lblcoords.Text = (funcaoescolhida + "\nF(x) = " + a + "*X" + b + "\n X = " + x.ToString() + "\n Y = " + y.ToString("f2"));
+            else if (funcaoescolhida == "Função Exponencial") lblcoords.Text = (funcaoescolhida + "\nF(X) = " + a + "*" + b + "^X\n X = " + x.ToString() + "\n Y = " + dexpoY.ToString("f2"));
+            else if (funcaoescolhida == "Função Quadrática") lblcoords.Text = (funcaoescolhida + "\nF(X) = " + a + "*X^2 + " + b + "*X + " + c + "\n X = " + x.ToString() + "\n Y = " + y.ToString("f2"));
+            else lblcoords.Text = ("Funcao nao escolhida");
+            
+
+        }
+
+        private void btnGrafico_Click(object sender, EventArgs e)
+        {
+            lblGrafico.Text = String.Empty;
+
+            string funcaoescolhida = cboFuncao.Text;
+            decimal decimalx = nudX.Value, decimala = nudA.Value, decimalb = nudB.Value, decimalc = nudC.Value;
+            float x = (float)decimalx, a = (float)decimala, b = (float)decimalb, c = (float)decimalc;
+            float expoY = 1, QuadX = 1;
+
+
+            //Numeros necessários para o cálculo das funções
+            double dx = (double)x, db = (double)b;
+            double dexpoY = (double)expoY;
+
+            for (int i = 0; i < 2; i++)//x ao quadrado
+            {
+                QuadX *= x;
+            }
+
+
+
+            if (funcaoescolhida == "Função Afim")//para ver a função escolhida
+            {
+                for (int i = 1; i < 12; i++)
+                {
+                    lblGrafico.Text += "\nX = " + (i - 1) + " |  Y = " + (a * (i-1) + b).ToString("f2");
+                }
+            }
+            else if (funcaoescolhida == "Função Exponencial")
+            {
+                for (int i = 1; i < 12; i++)
+                {
+                    lblGrafico.Text += "\nX = " + (i-1) + " |  Y = " + (Math.Pow(b, (i - 1)) * a).ToString("f2");
+                }
+            }
+            else if (funcaoescolhida == "Função Quadrática")
+            {
+                for (int i = 1; i < 12; i++)
+                {
+                    lblGrafico.Text += "\nX = " + (i - 1) + " |  Y = " + (a * (i - 1)*(i - 1) + (b * (i - 1)) + c).ToString("f2");
+                }
+            }
+            else lblGrafico.Text = ("Funcao nao escolhida");
+
+
+            
+            //-----------------------------------------------------------------------------------------------------
+            
+            Bitmap graficofeito = new Bitmap(720,720);
+            Graphics grafico = Graphics.FromImage(graficofeito); //classe desenha
+            Brush pintar = Brushes.BlueViolet, pintar1 = Brushes.Red, pintar2 = Brushes.Red;
+
+
+            int centrox = pictureBox1.Width/2, centroy = pictureBox1.Height/2;
+            
+            float pixel = 40;//quanto cada unidade de conta x da função vai valer na prática
+            float centroy1 = 0;
+            float ytela = 400;
+            float sla = 0, sla1 = 0, primerosla =0;
+            float xgrafico = 0, ygrafico = 1; //x sempre sera x, o valor que colocamos na funcao
+
+
+            //para fazer o plano cartesiano
+            for (int inicio = 0; inicio < 800; inicio += 1)//tamanho do pic
+            {   
+                centroy1 = centroy + inicio-400; //ex ja que centroy é 400 pois 800/2 é 400 logo o centro y inicial é zero
+                float xtela = centrox;
+                sla++;
+                primerosla++;
+
+
+                if (sla1 == 5 * 4 + 20)
+                {
+                    sla1 = 0;
+                    grafico.FillRectangle(pintar2, xtela, centroy1, 10, 4);
+                    grafico.FillRectangle(pintar2, centroy1, centrox, 4, 10);
+                }
+                if (sla == 1) {
+                    sla = 0;
+                    sla1 = sla1 + 1;
+                    grafico.FillRectangle(pintar1, xtela, centroy1, 4, 4);
+                    grafico.FillRectangle(pintar1, centroy1, centrox, 4, 4);
+                }
+                
+                
+
+
+            }
+
+            //para fazer a imagem da funcao
+            for (int inicio = -400; inicio < 400; inicio += 1)//ex quando pegar x do graf ele tem que ser 0 
+            {
+                xgrafico = inicio / pixel;
+                if (funcaoescolhida == "Função Afim") ygrafico = (a * xgrafico + b);
+                else if (funcaoescolhida == "Função Exponencial")
+                {
+                        float expo = (float)(Math.Pow(b, xgrafico) * a);
+                        ygrafico = expo;//quando x sobre função exponencial o x fica diferente
+                }
+                else if (funcaoescolhida == "Função Quadrática")
+                {
+                    ygrafico = (a * xgrafico * xgrafico) + (b * xgrafico) + c;
+                }  
+                float xtela = centrox + (xgrafico * pixel);
+                ytela = centroy - (ygrafico * pixel);
+                grafico.FillRectangle(pintar, xtela, ytela, 4, 4);
+                
+                
+            }
+            pictureBox1.Image = graficofeito;
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void nudB_ValueChanged(object sender, EventArgs e)
+        {
+            
+            
+        }
+    }
+}
+
